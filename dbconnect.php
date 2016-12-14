af193479ce3d09635d9b2074963669100b9ec70d
Tércio Garcia <tercio.garcia@smarti9.com.br>
2016-12-14 15:48:58 -0200

    Conexão

--- dbconnect.php
+++ dbconnect.php
@@ -1,3 +1,4 @@
 <?php
-	$database = new PDO('pgsql:dbname=trabalhobd host=localhost', 'postgres', '123456');
+	//$database = new PDO('pgsql:dbname=trabalhobd host=localhost', 'postgres', '123456');
+	$database = new PDO('pgsql:dbname=dfu2u1k3jutt86 host=ec2-54-225-195-254.compute-1.amazonaws.com', 'museeceorvqxlj', 'UWNiRSNWQaN9DgNBO7GHBeKr83');
 ?>
